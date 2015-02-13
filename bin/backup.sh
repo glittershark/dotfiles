b6ade2ec64cb9a4abc2a725fc0e5a7ee16ab394a
Griffin Smith <wildgriffin45@gmail.com>
2015-02-13 17:23:35 -0500

    [tarsnap] Nicer backup script
    Report errors in tarsnap backup script

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 for dir in $(< /home/griffin/.tarsnap/backup_dirs) ; do
-    nice -n 10 tarsnap -c -f $(hostname)-$(date -u +%Y%m%d-%H%M%S)-$(echo $dir | tr '/' '-') -C / $dir
+    if nice -n 10 tarsnap $@ -c -f $(hostname)-$(date -u +%Y%m%d-%H%M%S)-$(echo $dir | tr '/' '-') -C / $dir; then
+        notify-send "Backed up $dir"
+    else
+        notify-send "Error while backing up $dir"
+    fi
 done
 
 # Delete backups more than n days old
